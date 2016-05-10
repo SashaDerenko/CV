5c356d7971caf6da2a6af02474d4f5631a4aab88
Sasha <astra_97@ukr.net>
2016-05-10 00:19:02 +0000

    added formSpree

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-	$("#menu").on("click","a", function (event) {
+    $("body").on("click","a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href');
+        var top = $(id).offset().top;
+        
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+	$('#btn-send-mail').submit(function (event) {
+		$.ajax({
+			url: "https://formspree.io/astra_97@ukr.net",
+			method: "POST",
+			data: {
+				name: name-input.value,
+				email: email.value,
+				message: message.value
+			},
+			dataType: "json"
+		}).done(function () {
+			$('#mail-form').html('<h1>Thank you!</h1>');
+		});
 		event.preventDefault();
-
-		var id  = $(this).attr('href'),
-
-        top = $(id).offset().top;
-		
-		$('body,html').animate({scrollTop: top}, 1500);
 	});
 });
